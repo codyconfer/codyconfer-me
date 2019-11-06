ea12e4d934cb986578a54947443b2fd429f11d02
codyconfer <mail@codyconfer.me>
2019-11-05 23:59:35 -0700

    styling and structuring

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,26 @@
 <template>
-  <section>
+  <main class="container">
+    <Header></Header>
+    <Nav></Nav>
     <nuxt />
-  </section>
+    <Footer></Footer>
+  </main>
 </template>
+
+    Header,
+    Nav,
+    Footer
+
+<script>
+import Header from '~/components/Header.vue'
+import Nav from '~/components/Nav.vue'
+import Footer from '~/components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Nav,
+    Footer
+  },
+}
+</script>
